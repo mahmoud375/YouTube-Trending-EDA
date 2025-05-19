d030a3affad7eaf26a2cd0733bc24005ed7f3ddf
Mahmoud <star7ana@gmail.com>
2025-05-19 21:19:59 +0300

    test import from dir using config_loader

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,16 +1,13 @@
 import sys
 import os
+from pathlib import Path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from src.config_loader import get_paths_config
+from src.preprocessing.clean_data import load_data
 
-from pathlib import Path
-import yaml
-with open(os.path.join('config', 'paths.yaml'), 'r') as f:
-    paths = yaml.safe_load(f)
-
-from src.preprocessing.clean_data import load_data, split_data
-
-CA_data_pass = os.path.join(paths['CA_data'], 'CAvideos.csv')
-CA_data = load_data(CA_data_pass)
+paths = get_paths_config()
+CA_data_path = Path(paths['CA_data'])/ 'CAvideos.csv'
+CA_data = load_data(CA_data_path)
 
 print(CA_data.head())
